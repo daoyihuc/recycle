58fb91c73776be5a779f198b5bedd44a9e39f6fa
14760716236 <1939595471@qq.com>
2020-10-27 17:45:25 +0800

    daoyi

--- pages/invite/invite.js
+++ pages/invite/invite.js
@@ -1,7 +1,7 @@
 Page({
     data: {
         id: "",
-        bac_img: "https://cdn.jsdelivr.net/gh/daoyihuc/recycle@1.2/static/images/shareback.png",
+        bac_img: "https://cdn.jsdelivr.net/gh/daoyihuc/recycle@1.4/static/images/share.png",
         down_bac_img: "",
         isShow: !0,
         qrCode: ""
