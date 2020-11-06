e7ae6912eb449d8c83adac9377ae9d95e8ef5e46
daoyi <1966287146@qq.com>
2020-11-06 23:17:51 +0800

    登录添加位置

--- pages/login/login.js
+++ pages/login/login.js
@@ -6,15 +6,23 @@ var  a={
     gender: "",
     nickname: "",
     avatar: "",
-    inviteuid: ""
+    inviteuid: "",
+    district: ""
 };
 var that="";
+// 引入SDK核心类
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
 Page({
     data: {
-
+        address:"",
+        Country:""
     },
     onLoad: function (options) {
         that=this;
+        qqmapsdk = new QQMapWX({
+            key: 'ORNBZ-U2M3U-VGLV2-2DLG2-SUH7V-5IFPD'
+        });
     },
     login:function(){
         wxLogin().then( res =>{
@@ -22,8 +30,7 @@ Page({
                 toast("登录失败");
             }else{
                 a.Code=res.code;
-                that.http(a);
-
+                that.getLocationName();
             }
         });
     },
@@ -66,5 +73,78 @@ Page({
             }
 
         })
-    }
+    },
+    // 获取位置
+    getLocationName:function () {
+        wx.getLocation({
+            success: function(res) {
+                console.log(res)
+
+                // 调用sdk接口
+                qqmapsdk.reverseGeocoder({
+                    location: {
+                        latitude: res.latitude,
+                        longitude: res.longitude
+                    },
+                    success: function (res) {
+                        //获取当前地址成功
+                        console.log(res.result.address);
+                        that.getArea(res.result.address);
+                        that.setData({
+                            address: res.result.address
+                        });
+                    },
+                    fail: function (res) {
+                        console.log(res);
+                        // toast("地址获取成功");
+                    }
+                });
+            },
+        })
+    },
+    //省市区截取
+    getArea: function(str) {
+        let area = {}
+        let index11 = 0
+        let index1 = str.indexOf("省")
+        if (index1 == -1) {
+            index11 = str.indexOf("自治区")
+            if (index11 != -1) {
+                area.Province = str.substring(0, index11 + 3)
+            } else {
+                area.Province = str.substring(0, 0)
+            }
+        } else {
+            area.Province = str.substring(0, index1 + 1)
+        }
+
+        let index2 = str.indexOf("市")
+        if (index11 == -1) {
+            area.City = str.substring(index11 + 1, index2 + 1)
+        } else {
+            if (index11 == 0) {
+                area.City = str.substring(index1 + 1, index2 + 1)
+            } else {
+                area.City = str.substring(index11 + 3, index2 + 1)
+            }
+        }
+
+        let index3 = str.lastIndexOf("区")
+        if (index3 == -1) {
+            index3 = str.indexOf("县")
+            area.Country = str.substring(index2 + 1, index3 + 1)
+        } else {
+            area.Country = str.substring(index2 + 1, index3 + 1)
+        }
+        if(index3<str.length){
+            area.street = str.substring(index3+1,str.length);
+        }
+        this.setData({
+            Country: area.Country,
+        });
+        a.district=area.Country;
+        that.http(a);
+
+        return area;
+    },
 });
