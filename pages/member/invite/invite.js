44214c49ed1f2244493bdeb82a658adb183141c1
daoyi <1966287146@qq.com>
2020-11-09 21:23:00 +0800

    我的邀请

--- pages/member/invite/invite.js
+++ pages/member/invite/invite.js
@@ -17,6 +17,7 @@ Page({
     onLoad: function (options) {
 
         that=this;
+        this.handleSetting();
     },
     onShow:function(){
         aData.token=wx.getStorageSync("token");
@@ -27,8 +28,13 @@ Page({
         PoliteInvitation(params).then(res=>{
             console.log(res);
             if(res.code===1){
+                const a={
+                    erweima: "https://fg.996sh.com/erweima/erweima1.png",
+                    share_img: "http://fgadmin.996sh.com/uploads/20201109/843b797a8a5b7e537446b842c712f0bf.jpg"
+                }
                 that.setData({
-                    qrCode: res.data
+                    qrCode: res.data.erweima,
+                    bac_img: res.data.share_img
                 });
             }else{
                 toast(res.msg,1);
@@ -49,6 +55,8 @@ Page({
     },
     // 保存图片
     saveImg:function(e){
+
+
         let that = this;
 
         //获取相册授权
@@ -70,14 +78,17 @@ Page({
                 } else {//用户已经授权过了
                     that.saveImgToLocal();
                 }
+            },fail:function () {
+                that.saveImgToLocal();
             }
         })
 
     },
     saveImgToLocal: function (e) {
         let that = this;
-
+        toast("开始保存",0);
         let imgSrc = that.data.qrCode;
+        console.log(that.data.qrCode);
         wx.downloadFile({
             url: imgSrc,
             success: function (res) {
@@ -92,6 +103,13 @@ Page({
                             duration: 2000
                         })
                     },
+                    fail:function () {
+                        wx.showToast({
+                            title: '保存失败',
+                            icon: 'success',
+                            duration: 2000
+                        })
+                    }
                 })
             }
         })
@@ -104,20 +122,20 @@ Page({
         // 对用户的设置进行判断，如果没有授权，即使用户返回到保存页面，显示的也是“去授权”按钮；同意授权之后才显示保存按钮
 
         if (!e.detail.authSetting['scope.writePhotosAlbum']) {
-            // wx.showModal({
-            //   title: '警告',
-            //   content: '若不打开授权，则无法将图片保存在相册中！',
-            //   showCancel: false
-            // })
+            wx.showModal({
+              title: '警告',
+              content: '若不打开授权，则无法将图片保存在相册中！',
+              showCancel: false
+            })
             that.setData({
                 openSettingBtnHidden: false
             })
         } else {
-            // wx.showModal({
-            //   title: '提示',
-            //   content: '您已授权，赶紧将图片保存在相册中吧！',
-            //   showCancel: false
-            // })
+            wx.showModal({
+              title: '提示',
+              content: '您已授权，赶紧将图片保存在相册中吧！',
+              showCancel: false
+            })
             that.setData({
                 openSettingBtnHidden: true
             })
